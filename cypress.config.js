d52c17ddb4c5e91166025d8045d27769059ba280
yusuf <yusuf@KING27.com>
2024-09-06 13:40:18 +0700

    perbaikan script

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     specPattern: [
-      "cypress/e2e/01_login/**",
-      "cypress/e2e/02_dashboard/**",
-      "cypress/e2e/03_cart/**",
+      "cypress/e2e/01_login/*cy.js",
+      "cypress/e2e/02_dashboard/*cy.js",
+      "cypress/e2e/03_cart/*cy.js",
     ],
 
     setupNodeEvents(on, config) {
